754a5e1114d023ed7095e5cb38c61ab9770b3df1
Schildkroete1 <jonas.10@gmx.de>
2018-05-06 18:16:35 +0200

    Create id.py

---
+++ id.py
@@ -0,0 +1,23 @@
+import MySQLdb
+import sys
+from hashids import Hashids
+
+hashids = Hashids()
+
+def insert():
+
+    client = MySQLdb.connect(host="localhost", user="", passwd="", db="shorter")
+    cursor = client.cursor()
+    sql = "SELECT id FROM id WHERE used=0"
+    client.query(sql)
+    result=client.store_result()
+    temp = result.fetch_row()[0][0]
+    print hashids.encode(int(temp), int(temp))
+    cursor.execute ("UPDATE id SET used=1 WHERE used=0")
+    client.commit()
+    query = "INSERT INTO id(id) values(\"" + str(int(temp) + 1) + "\")"
+    cursor = client.cursor()
+    cursor.execute(query)
+    client.commit()
+    client.close()
+insert()
